cd44a751734ff11370f063f0fdab2b51b1555a49
System Administrator <rpr.bbrepo202205@gmail.com>
2022-11-28 16:58:58 -0500

    Verifying can read the data file

--- 2021/01/check_depth/src/main.rs
+++ 2021/01/check_depth/src/main.rs
@@ -4,5 +4,5 @@ fn main() {
     let contents = fs::read("depth_readings.txt")
         .expect("Should have been able to read this file");
 
-    println!("Contents of file is:\n{contents}");
+    dbg!(contents);
 }
